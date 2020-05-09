a8cdee77102e616f9857c2e37ad1ed76d66907b5
carlo <carlo@DESKTOP-6U13FNB.home>
2020-05-10 01:32:47 +0200

    Se añaden las clases "user" y "role"

--- YourGamingStory/src/main/java/com/example/demo/YourGamingStoryApplication.java
+++ YourGamingStory/src/main/java/com/example/demo/YourGamingStoryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class YourGamingStoryApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(YourGamingStoryApplication.class, args);
 	}
 
